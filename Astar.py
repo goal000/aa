78257888e9a7f5b156452d2fa68d094134b50e99
goal000 <118476290+goal000@users.noreply.github.com>
2022-12-08 00:14:07 +0530

    Update Astar.py

--- Astar.py
+++ Astar.py
@@ -1,47 +1,37 @@
-def sort(stk,cost,top):
-    for i in range(0,top):
-        for j in range(0,top):
-            if(cost[j]<cost[j+1]):
-                temp=cost[j]
-                cost[j]=cost[j+1]
-                cost[j+1]=temp
-                temp=stk[j]
-                stk[j]=stk[j+1]
-                stk[j+1]=temp
-print("Enter no of vertices : ")
-n=int(input())
-a=[]
-par=[0]*100
-for i in range(0,n):
-    b=[0]*n
-    a.append(b)
-print("enter heuristic value fr every node(node,heuristic value)")
-h=[0]*100
-for i in range(0,n):
-    src,h1=map(int,input().split(' '))
-    h[src]=h1
-print("enter no of edges : ")
-edgs=int(input())
-print("Enter edges from src to dest with cost: ")
-for i in range(0,edgs):
-    src,dest,cost=map(int,input().split(' '))
-    a[src][dest]=cost
-top=0
-stk=[0]*n
-cost=[0]*n
-print("Enter src and dest : ")
-src,dest=map(int,input().split(' '))
-stk[top]=src
-while(stk[top]!=dest):
-    print(stk[top],end=" ")
-    ele=stk[top]
-    top-=1
-    for i in range(0,n):
-        if(a[ele][i]>0):
-            top+=1;
-            stk.insert(0,i)
-            cost.insert(0,a[ele][i]+h[i]+par[ele])
-            par[i]=par[ele]+a[ele][i]
-    if(ele!=stk[top]):
-        sort(stk,cost,top)
-print(dest)
+ from queue import PriorityQueue
+def a_star(v,l,goal,h):
+ open_list=PriorityQueue()
+ open_list.put((13,0))
+ flag=0
+ path=[]
+ while not(open_list.empty()) and flag==0:
+ 	t=open_list.get()
+ 	curr_cost=t[0]
+ 	curr=t[1]
+ 	path.append(curr)
+ 	if(curr==goal):
+ 		flag=1
+ 		break
+ 	for i in range(v):
+ 		if l[curr][i]!=-1:
+ 			cost=l[curr][i]
+ 			open_list.put((curr_cost-h[curr]+cost+h[i],i))
+ if flag==1:
+ 	print('Goal Node is found .The Path is : ',path)
+ else :
+ 	print('Goal Node is not found')
+
+v=int(input('Enter the number of Nodes : '))
+l=[[-1 for i in range(v)] for j in range(v)]
+edge=int(input('Enter no of edges : '))
+for i in range(edge):
+	print('Enter the start ,end , g(n) (Cost) of edge -',i+1)
+	start, end, cost=[int(x) for x in input().split()]
+	l[start][end]=cost
+h={}
+for i in range(v):
+	print('Enter h(n) for Node',i+1)
+	x=int(input())
+	h[i]=x
+goal=int(input('Enter the goal node :'))
+a_star(v,l,goal,h)
