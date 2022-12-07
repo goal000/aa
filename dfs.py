6a63e909dc20c19894702850a3e684a789e4ac7f
goal000 <118476290+goal000@users.noreply.github.com>
2022-12-08 00:18:12 +0530

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -1,21 +1,18 @@
-def dfs(vis,a,r1,n):
+def dfs(vis,a,src,n):
     for i in range(0,n):
-        if(a[r1][i]==1 and vis[i]==0):
+        if(a[src][i]==1 and vis[i]==0):
             print(i,end=" ")
             vis[i]=1
             dfs(vis,a,i,n)
 print("Enter no of vertices : ")
 n=int(input())
-a=[]
-for i in range(0,n):
-    b=[0]*n
-    a.append(b)
+a=[[0 for i in range(n)] for j in range(n)]
 print("Enter no of edgs : ")
 edgs=int(input())
 vis=[0]*n
 print("Enter edges (src to dst) : ")
 for i in range(0,edgs):
-    l1,l2=map(int,input().split(' '))
+    l1,l2=[int(x) for x in input().split()]
     a[l1][l2]=1
 print(0,end=" ")
 dfs(vis,a,0,n)
